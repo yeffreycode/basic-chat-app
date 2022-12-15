2b71c0445b8f5e81281f72d715bd712affe21855
yeffreycode <yeffreyespinoza200@gmail.com>
2022-12-15 10:19:17 -0500

    update 1

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ app.use('/', express.static(path.join(__dirname, './public')));
 
 const httpServer = http.createServer(app);
 
+let users = [];
+
 const io = new Server(httpServer);
 io.on('connection', (socket)=> {
-	console.log({id: socket.id});
+	console.log('new user connected: ' +socket.id);
+	socket.on('login', data=> {
+		let usernameExists = users.some(el => el.username === data.username);
+		if (!usernameExists) {
+			users.push({socketId: socket.id, ...data});
+			socket.emit('login', true);
+		}else {
+			socket.emit('login', false);
+		}
+	})
 });
 
 module.exports = httpServer;
--- public/script.js
+++ public/script.js
@@ -1,4 +1,58 @@
 const socket  = io();
-socket.on('connect', ()=> {
-	alert(socket.id)
+const app = {};
+const d = document;
+
+app.login = (socket, username) => {
+	socket.emit('login', {username})
+}
+app.validateUsername = (username) => {
+	let regex = /^[a-z0-9]+$/;
+	return regex.test(username);
+}
+app.loginLoading = () => {
+	let login = d.querySelector('.login');
+	login.classList.add('loading');
+}
+app.endLoginLoading = () => {
+	let login = d.querySelector('.login');
+	login.classList.remove('loading');
+};
+app.loginError=(errStr) => {
+	let loginError = d.querySelector('.login_error');
+	loginError.innerHTML = errStr;
+}
+//open layouts
+app.openUsersList = () => {
+	d.querySelector(".login").style.display = 'none';
+	d.querySelector('.chat').style.display = 'none';
+	d.querySelector('.users').style.display = 'flex';
+}
+
+app.bindLoginForm = () => {
+	let loginForm = d.getElementById('login_form');
+	let username = d.getElementById('username_input');
+	loginForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		if (!app.validateUsername(username.value)) {
+			alert('username only contain lowercase letters and numbers');
+			return;
+		}
+		app.loginLoading();
+		app.login(socket, username.value);
+	})
+}
+
+//sockets listen
+socket.on('login', d => {
+	app.endLoginLoading();
+	if (!d) {
+		return app.loginError('* Username already exists');
+	}
+	app.openUsersList();
 });
+
+
+window.onload = () => {
+	app.bindLoginForm();
+}
+
