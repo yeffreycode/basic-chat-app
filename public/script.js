61a165dafebacf89b170c16b029bb5379d154273
yeffreycode <yeffreyespinoza200@gmail.com>
2022-12-16 09:37:31 -0500

    message component

--- public/script.js
+++ public/script.js
@@ -1,58 +1,53 @@
-const socket  = io();
+const socket = io();
 const app = {};
 const d = document;
 
 app.login = (socket, username) => {
-	socket.emit('login', {username})
-}
-app.validateUsername = (username) => {
-	let regex = /^[a-z0-9]+$/;
-	return regex.test(username);
-}
-app.loginLoading = () => {
-	let login = d.querySelector('.login');
-	login.classList.add('loading');
-}
-app.endLoginLoading = () => {
-	let login = d.querySelector('.login');
-	login.classList.remove('loading');
+  socket.emit("login", { username });
 };
-app.loginError=(errStr) => {
-	let loginError = d.querySelector('.login_error');
-	loginError.innerHTML = errStr;
-}
+app.validateUsername = (username) => {
+  let regex = /^[a-z0-9]+$/;
+  return regex.test(username);
+};
+app.loginLoading = () => {
+  d.querySelector(".login").classList.add("loading");
+};
+app.endLoginLoading = () => {
+  d.querySelector(".login").classList.remove("loading");
+};
+app.loginError = (errStr) => {
+  d.querySelector(".login_error").innerHTML = errStr;
+};
 //open layouts
 app.openUsersList = () => {
-	d.querySelector(".login").style.display = 'none';
-	d.querySelector('.chat').style.display = 'none';
-	d.querySelector('.users').style.display = 'flex';
-}
+  d.querySelector(".login").style.display = "none";
+  d.querySelector(".chat").style.display = "none";
+  d.querySelector(".users").style.display = "block";
+};
 
 app.bindLoginForm = () => {
-	let loginForm = d.getElementById('login_form');
-	let username = d.getElementById('username_input');
-	loginForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		if (!app.validateUsername(username.value)) {
-			alert('username only contain lowercase letters and numbers');
-			return;
-		}
-		app.loginLoading();
-		app.login(socket, username.value);
-	})
-}
+  let loginForm = d.getElementById("login_form");
+  let username = d.getElementById("username_input");
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!app.validateUsername(username.value)) {
+      alert("username only contain lowercase letters and numbers");
+      return;
+    }
+    app.loginLoading();
+    app.login(socket, username.value);
+  });
+};
 
 //sockets listen
-socket.on('login', d => {
-	app.endLoginLoading();
-	if (!d) {
-		return app.loginError('* Username already exists');
-	}
-	app.openUsersList();
+socket.on("login", (d) => {
+  app.endLoginLoading();
+  if (!d) {
+    return app.loginError("* Username already exists");
+  }
+  app.openUsersList();
 });
 
-
 window.onload = () => {
-	app.bindLoginForm();
-}
-
+  app.bindLoginForm();
+};
