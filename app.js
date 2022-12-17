1312ed455db765d3caf417209976feb4a1e2a978
yeffreycode <yeffreyespinoza200@gmail.com>
2022-12-17 00:39:17 -0500

    chat functional

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 const express = require("express");
-const {Server} = require('socket.io');
-const path = require('path');
-const http = require('http');
+const { Server } = require("socket.io");
+const path = require("path");
+const http = require("http");
 
 const app = express();
-app.use('/', express.static(path.join(__dirname, './public')));
+app.use("/", express.static(path.join(__dirname, "./public")));
 
 const httpServer = http.createServer(app);
 
 let users = [];
 
+//remove user
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
 const io = new Server(httpServer);
-io.on('connection', (socket)=> {
-	console.log('new user connected: ' +socket.id);
-	socket.on('login', data=> {
-		let usernameExists = users.some(el => el.username === data.username);
-		if (!usernameExists) {
-			users.push({socketId: socket.id, ...data});
-			socket.emit('login', true);
-		}else {
-			socket.emit('login', false);
-		}
-	})
+io.on("connection", (socket) => {
+  console.log("new user connected: " + socket.id);
+  socket.on("login", (data) => {
+    let usernameExists = users.some((el) => el.username === data.username);
+    if (!usernameExists) {
+      let user = { socketId: socket.id, ...data };
+      users.push({ socketId: socket.id, ...data });
+      socket.emit("login", user);
+      users.map((item) => {
+        item.socketId !== user.socketId && io.to(item.socketId).emit("new-user", user);
+      });
+    } else {
+      socket.emit("login", false);
+    }
+  });
+  socket.on("get-users", () => {
+    socket.emit("get-users", users);
+  });
+
+  //message
+  socket.on("new-message", (msg) => {
+    io.to(msg.receiverId).emit("new-message", msg);
+    msg.senderId !== msg.receiverId && socket.emit("new-message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    removeUser(socket.id);
+    users.map((user) => {
+      io.to(user.socketId).emit("remove-user", socket.id);
+    });
+  });
 });
 
 module.exports = httpServer;
-
--- public/script.js
+++ public/script.js
@@ -2,6 +2,13 @@ const socket = io();
 const app = {};
 const d = document;
 
+app.state = {
+  username: "",
+  socketId: "",
+  users: [],
+  currentChatId: "",
+};
+
 app.login = (socket, username) => {
   socket.emit("login", { username });
 };
@@ -18,6 +25,12 @@ app.endLoginLoading = () => {
 app.loginError = (errStr) => {
   d.querySelector(".login_error").innerHTML = errStr;
 };
+app.setUser = (user) => {
+  app.state.socketId = user.socketId;
+  app.state.username = user.username;
+  let p = d.querySelector(".h-users p");
+  p.innerHTML = app.state.username;
+};
 //open layouts
 app.openUsersList = () => {
   d.querySelector(".login").style.display = "none";
@@ -25,6 +38,13 @@ app.openUsersList = () => {
   d.querySelector(".users").style.display = "block";
 };
 
+app.openChat = () => {
+  d.querySelector(".login").style.display = "none";
+  d.querySelector(".chat").style.display = "flex";
+  d.querySelector(".users").style.display = "none";
+};
+
+//bind login form
 app.bindLoginForm = () => {
   let loginForm = d.getElementById("login_form");
   let username = d.getElementById("username_input");
@@ -39,15 +59,151 @@ app.bindLoginForm = () => {
   });
 };
 
+//bind message form
+app.bindMessageForm = () => {
+  let messageForm = d.getElementById("chat_form");
+  let txtMessage = d.getElementById("input_message");
+  messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!txtMessage.value) {
+      alert("message empty");
+      return;
+    }
+    let msg = app.messageObject(app.state.socketId, app.state.currentChatId, txtMessage.value);
+    app.sendMessage(msg);
+    txtMessage.value = "";
+  });
+};
+
+//users list
+app.generateUser = (user) => {
+  let { username, socketId } = user;
+  let div = d.createElement("div");
+  let span = d.createElement("span");
+  let btn = d.createElement("button");
+  //add atributes
+  div.classList.add("user");
+  div.setAttribute("id", socketId);
+  btn.setAttribute("id", socketId);
+  btn.classList.add("user-btn");
+  //add username to span
+  let usernameTxt = d.createTextNode(username);
+  span.appendChild(usernameTxt);
+  //add chat text to btn
+  let btnTxt = d.createTextNode("chat");
+  btn.appendChild(btnTxt);
+  //add childs to div
+  div.appendChild(span);
+  div.appendChild(btn);
+  return div;
+};
+app.getUsers = () => {
+  socket.emit("get-users");
+};
+app.getUser = () => {};
+app.addUsersList = (user) => {
+  let div = d.querySelector(".users-list");
+  div.appendChild(user);
+};
+app.removeUser = (socketId) => {
+  let listUsers = d.querySelector(".users-list");
+  let child = d.getElementById(socketId);
+  listUsers.removeChild(child);
+};
+
+//chat
+app.bindOpenChat = () => {
+  if (!d.querySelectorAll(".user-btn")) return;
+  d.querySelectorAll(".user-btn").forEach((btn) => {
+    btn.onclick = (e) => {
+      app.setCurrentChat(e.target.id);
+      app.openChat();
+    };
+  });
+};
+app.setUsernameToChatHeader = (username) => {
+  let currentUsername = document.getElementById("current-username");
+  currentUsername.innerHTML = username;
+};
+app.setCurrentChat = (socketId) => {
+  app.state.currentChatId = socketId;
+  let user = app.state.users.find((user) => user.socketId === socketId);
+  app.setUsernameToChatHeader(user.username);
+};
+
+//messages
+app.messageObject = (senderId, receiverId, txtMessage) => {
+  return { senderId, receiverId, txtMessage };
+};
+app.sendMessage = (messageObject) => {
+  socket.emit("new-message", messageObject);
+};
+app.generateMessage = (msg) => {
+  let div = d.createElement("div");
+  let spanTik = d.createElement("span");
+  let spanTxt = d.createElement("span");
+  //add clasess
+  let classTxt = msg.senderId == app.state.socketId ? "message own" : "message";
+  div.setAttribute("class", classTxt);
+  spanTik.classList.add("tik");
+  spanTxt.classList.add("txt");
+  //create content message
+  let txt = d.createTextNode(msg.txtMessage);
+  spanTxt.appendChild(txt);
+
+  //set to container
+  div.appendChild(spanTik);
+  div.appendChild(spanTxt);
+  return div;
+};
+app.addMessage = (message) => {
+  let messages = d.querySelector(".messages");
+  messages.appendChild(message);
+};
 //sockets listen
+
+//login
 socket.on("login", (d) => {
   app.endLoginLoading();
   if (!d) {
     return app.loginError("* Username already exists");
   }
+  app.setUser(d);
   app.openUsersList();
+  app.getUsers();
+});
+
+//new user
+socket.on("new-user", (user) => {
+  let element = app.generateUser(user);
+  app.addUsersList(element);
+  app.state.users.push(user);
+  app.bindOpenChat();
+});
+//remove user
+socket.on("remove-user", (id) => {
+  app.removeUser(id);
+});
+//get users
+socket.on("get-users", (users) => {
+  if (users && users.length) {
+    users.forEach((user) => {
+      let element = app.generateUser(user);
+      app.addUsersList(element);
+    });
+    app.state.users = users;
+    app.bindOpenChat();
+  }
+});
+
+//message
+socket.on("new-message", (msg) => {
+  let msgDiv = app.generateMessage(msg);
+  (app.state.currentChatId === msg.senderId || app.state.socketId === msg.senderId) && app.addMessage(msgDiv);
+  msgDiv.scrollIntoView();
 });
 
 window.onload = () => {
   app.bindLoginForm();
+  app.bindMessageForm();
 };
